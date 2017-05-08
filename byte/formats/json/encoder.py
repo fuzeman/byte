05a72223c82dd9d022aeb591d3b078ad2c9a1084
Dean Gardiner <me@dgardiner.net>
2017-05-08 15:58:22 +1200

    Fixed Python 3+ compatibility issues in the json encoder

--- byte/formats/json/encoder.py
+++ byte/formats/json/encoder.py
@@ -1,4 +1,5 @@
 from json import JSONEncoder
+import six
 
 
 class JsonEncoder(object):
@@ -9,6 +10,9 @@ class JsonEncoder(object):
 
     def write_dict(self, items):
         for chunk in self.encoder.iterencode(DictionaryEmitter(items)):
+            if six.PY3:
+                chunk = bytes(chunk, encoding='utf8')
+
             self.stream.write(chunk)
 
     def write_list(self, items):
@@ -16,20 +20,29 @@ class JsonEncoder(object):
 
 
 class DictionaryEmitter(dict):
-    def __init__(self, items, **kwargs):
-        super(DictionaryEmitter, self).__init__(**kwargs)
+    def __init__(self, items):
+        super(DictionaryEmitter, self).__init__()
 
         self._items = items
 
     def items(self):
-        raise Exception('DictionaryEmitter.items() is not supported')
+        if six.PY2:
+            raise Exception('DictionaryEmitter.items() is not supported')
+
+        return self.iteritems()
 
     def iteritems(self):
         for item in self._items:
             yield item
 
+    def __bool__(self):
+        return True
+
     def __nonzero__(self):
         return True
+
+    def __repr__(self):
+        return 'DictionaryEmitter(%r)' % (self._items,)
 
 
 class ListEmitter(object):
