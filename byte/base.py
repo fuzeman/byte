5762885a9a553e42cc94bceb3ffe7324f025072f
Dean Gardiner <me@dgardiner.net>
2017-05-08 16:00:14 +1200

    Added documentation to [byte/base.py]

--- byte/base.py
+++ byte/base.py
@@ -1,11 +1,33 @@
 class BaseExpression(object):
+    """Base class for expressions."""
+
     def execute(self, item):
+        """Execute expression.
+        
+        :param item: Item
+        :type item: byte.model.Model
+        """
         raise NotImplementedError
 
 
 class BaseProperty(object):
+    """Base class for properties."""
+
     def get(self, obj):
+        """Get property value from object.
+        
+        :param obj: Item
+        :type obj: byte.model.Model
+        """
         raise NotImplementedError
 
     def set(self, obj, value):
+        """Set property value on object.
+        
+        :param obj: Item
+        :type obj: byte.model.Model
+        
+        :param value: Value
+        :type value: any
+        """
         raise NotImplementedError
