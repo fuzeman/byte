4500e5897caa0515acc4b129b5b24be27c041f7b
Dean Gardiner <me@dgardiner.net>
2017-05-15 11:59:46 +1200

    Removed the `Node.__str__` method

--- byte/core/models/nodes/base.py
+++ byte/core/models/nodes/base.py
@@ -1,7 +1,3 @@
 class Node(object):
     def compile(self):
         raise NotImplementedError('%s.compile() hasn\'t been implemented' % (self.__class__.__name__,))
-
-    def __str__(self):
-        statement, _ = self.compile()
-        return statement
