5c2c61320b8245a39a2525f691a2fe58bf7aaa1e
Dean Gardiner <me@dgardiner.net>
2017-05-16 16:08:34 +1200

    Added the `PYTHON_IMPLEMENTATION` and `PYPY*` attributes to the [byte.core.compat] module

--- byte/core/compat.py
+++ byte/core/compat.py
@@ -1,8 +1,9 @@
 """byte - compatibility module."""
 from __future__ import absolute_import, division, print_function
 
-import sys
+from byte.core.enums import PythonImplementation
 
+import sys
 
 PY2 = sys.version_info[0] == 2
 PY26 = sys.version_info[0:2] == (2, 6)
@@ -10,3 +11,28 @@ PY27 = sys.version_info[0:2] == (2, 7)
 
 PY3 = sys.version_info[0] == 3
 PY34 = sys.version_info[0:2] == (3, 4)
+
+PYPY = hasattr(sys, 'pypy_translation_info')
+PYPY2 = PYPY and PY27
+PYPY3 = PYPY and PY3
+
+
+# Python Implementation
+def detect_python_implementation():
+    if PYPY:
+        return PythonImplementation.PyPy
+
+    # Detect with the `platform` module
+    try:
+        import platform
+
+        if platform.python_implementation() == 'CPython':
+            return PythonImplementation.CPython
+
+        if platform.python_implementation() == 'PyPy':
+            return PythonImplementation.PyPy
+    except Exception:
+        return PythonImplementation.Unknown
+
+
+PYTHON_IMPLEMENTATION = detect_python_implementation()
---
+++ byte/core/enums.py
@@ -0,0 +1,5 @@
+class PythonImplementation(object):
+    Unknown = 0
+
+    CPython = 1
+    PyPy = 2
