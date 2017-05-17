37008645e80aa4ae11d69260b0bc7a25a6f38e81
Dean Gardiner <me@dgardiner.net>
2017-05-17 16:51:43 +1200

    core: compat: removed unneeded `PYTHON_IMPLEMENTATION` attribute

--- byte/core/compat.py
+++ byte/core/compat.py
@@ -1,8 +1,6 @@
 """byte - compatibility module."""
 from __future__ import absolute_import, division, print_function
 
-from byte.core.enums import PythonImplementation
-
 import sys
 
 PY2 = sys.version_info[0] == 2
@@ -15,24 +13,3 @@ PY34 = sys.version_info[0:2] == (3, 4)
 PYPY = hasattr(sys, 'pypy_translation_info')
 PYPY2 = PYPY and PY27
 PYPY3 = PYPY and PY3
-
-
-# Python Implementation
-def detect_python_implementation():
-    if PYPY:
-        return PythonImplementation.PyPy
-
-    # Detect with the `platform` module
-    try:
-        import platform
-
-        if platform.python_implementation() == 'CPython':
-            return PythonImplementation.CPython
-
-        if platform.python_implementation() == 'PyPy':
-            return PythonImplementation.PyPy
-    except Exception:
-        return PythonImplementation.Unknown
-
-
-PYTHON_IMPLEMENTATION = detect_python_implementation()
