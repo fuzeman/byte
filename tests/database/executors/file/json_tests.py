5a17e6de83040a86ee6ec2a66da598e00b947128
Dean Gardiner <me@dgardiner.net>
2017-06-27 17:50:21 +1200

    Added database tests

---
+++ tests/database/executors/file/json_tests.py
@@ -0,0 +1,46 @@
+from __future__ import absolute_import, division, print_function
+
+from byte.table import Table
+from byte.database import Database
+from tests.base.core.fixtures import get_fixture_uri
+from tests.base.models.dynamic.album import Album
+from tests.base.models.dynamic.artist import Artist
+from tests.base.models.dynamic.track import Track
+import byte.compilers.operation
+import byte.executors.file
+
+from hamcrest import *
+
+
+def test_get():
+    """Test collection with dynamic models."""
+    with get_fixture_uri('databases/music', scheme='file.json') as database_uri:
+        database = Database(database_uri, [
+            Table(Artist, name='artists'),
+            Table(Album, name='albums'),
+            Table(Track, name='tracks')
+        ], plugins=[
+            byte.compilers.operation,
+            byte.executors.file
+        ])
+
+        # Configure relations
+        database['albums'].connect(
+            artist=database['artists']
+        )
+
+        database['tracks'].connect(
+            artist=database['artists'],
+            album=database['albums']
+        )
+
+        # Fetch album, and validate properties
+        album = database['albums'].get(Album['id'] == 1)
+
+        assert album
+        assert album.id == 1
+        assert album.title == 'Humanz'
+
+        assert album.artist
+        assert album.artist.id == 1
+        assert album.artist.title == 'Gorillaz'
---
+++ tests/database/executors/memory/basic_tests.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import, division, print_function
+
+from byte.table import Table
+from byte.database import Database
+from tests.base.models.dynamic.artist import Artist
+import byte.compilers.operation
+import byte.executors.memory
+
+
+def test_basic():
+    """Test collection with dynamic models."""
+    database = Database('memory://', [
+        Table(Artist, name='artists')
+    ], plugins=[
+        byte.compilers.operation,
+        byte.executors.memory
+    ])
+
+    # Insert item
+    database['artists'].executor.items.update({
+        1: {
+            'id': 1,
+            'title': 'Daft Punk'
+        }
+    })
+
+    # Fetch artist, and validate properties
+    artist = database['artists'].get(Artist['id'] == 1)
+
+    assert artist
+    assert artist.id == 1
+    assert artist.title == 'Daft Punk'
